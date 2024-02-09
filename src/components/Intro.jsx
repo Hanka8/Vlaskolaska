3712f1109b11749d6a6839a1b492f0a0dce9ed64
Hanka8 <hanka.maruskevicova@gmail.com>
2024-02-09 11:36:27 +0100

    intro

---
+++ src/components/Intro.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+
+export default function Intro() {
+    const [currentFrame, setCurrentFrame] = useState(1);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentFrame(prevFrame => {
+                if (prevFrame === 4) {
+                    clearInterval(intervalId);
+                    return prevFrame;
+                } else {
+                    return prevFrame + 1;
+                }
+            });
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    return (
+        <div className='intro-box'>
+            <div className='intro'>
+                <div className='intro-frame' style={{ display: currentFrame === 1 ? 'block' : 'none' }}>
+                    <img className='intro-img fade-in' src="./images/intro/1.svg" alt="illustration" />
+                </div>
+                <div className='intro-frame' style={{ display: currentFrame === 2 ? 'block' : 'none' }}>
+                    <img className='intro-img fade-in' src="./images/intro/2.svg" alt="illustration" />
+                    <img className='intro-img' src="./images/intro/1.svg" alt="illustration" />
+                </div>
+                <div className='intro-frame' style={{ display: currentFrame === 3 ? 'block' : 'none' }}>
+                    <img className='intro-img' src="./images/intro/2.svg" alt="illustration" />
+                    <img className='intro-img' src="./images/intro/1.svg" alt="illustration" />
+                    <img className='intro-img bounce' src="./images/intro/3.svg" alt="illustration" />
+                </div>
+                <div className='intro-frame' style={{ display: currentFrame === 4 ? 'block' : 'none' }}>
+                    <img className='intro-img' src="./images/intro/2.svg" alt="illustration" />
+                    <img className='intro-img' src="./images/intro/1.svg" alt="illustration" />
+                    <img className='intro-img' src="./images/intro/3.svg" alt="illustration" />
+                    <img className='intro-img bounce' src="./images/intro/4.svg" alt="illustration" />
+                </div>
+            </div>
+        </div>
+    );
+}
