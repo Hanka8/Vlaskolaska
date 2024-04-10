787cc341cd668971001bc5b09807299a9af2faa0
Hanka8 <hanka.maruskevicova@gmail.com>
2024-04-10 17:50:21 +0200

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,29 +9,27 @@ export default function Contact() {
   return (
     <>
       <SwipeableTemporaryDrawer />
-      <div className='page'>
+      <div className='page contact'>
           <div className='page-logo'>
               <img src="images/contact.svg" alt="kontakt"/>
           </div>
-          <div className='page-content'>
-              <p className='h-2'>Karolína Čechová, IČO: 21104387</p>
+          <div className='page-content contact'>
+              <p className='text big emphasized contact'>Karolína Čechová</p>
+              <p className='text big emphasized contact'>IČO: 21104387</p>
+              <a className='link' href="mailto:karolinacechova22@gmail.com">karolinacechova22@gmail.com</a>
+                {/* <MdAlternateEmail size={30}/> */}
               <div className='contacts-flex mt-2'>
-                <FaWhatsapp size={30}/>
-                <a className='link' href="https://api.whatsapp.com/send?phone=732928740">+420732928740</a>
+                
+                <a className='link-ico' href="https://api.whatsapp.com/send?phone=732928740">
+                  <FaWhatsapp size={30}/>
+                </a>
+                <a className='link-ico' href="https://www.instagram.com/vlaskolaska/">
+                  <FaInstagram size={30}/>
+                </a>
+                <a className='link-ico' href='https://maps.app.goo.gl/CAkFdRvhrEF8vnvq9'>
+                  <SlLocationPin size={30}/>
+                </a>
               </div>
-              <div className='contacts-flex'>
-                <MdAlternateEmail size={30}/>
-                <a className='link' href="mailto:karolinacechova22@gmail.com">karolinacechova22@gmail.com</a>
-              </div>
-              <div className='contacts-flex'>
-                <FaInstagram size={30}/>
-                <a className='link' href="https://www.instagram.com/vlaskolaska/">vlaskolaska</a>
-              </div>
-              <div className='contacts-flex'>
-                <SlLocationPin size={30}/>
-                <a className='link' href='https://maps.app.goo.gl/CAkFdRvhrEF8vnvq9'>Praha 13</a>
-              </div>
-
           </div>
       </div>
     </>
