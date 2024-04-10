9f44f929e95ac1813de9ab67fdef1ff678197b09
Hanka8 <hanka.maruskevicova@gmail.com>
2024-04-10 18:37:48 +0200

    hover states

--- src/components/Landing/Intro.jsx
+++ src/components/Landing/Intro.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import SwipeableTemporaryDrawer from '../SwipableTemporaryDrawer';
+import { Link } from 'react-router-dom'
 
 export default function Intro() {
     const [currentFrame, setCurrentFrame] = useState(1);
@@ -22,7 +23,7 @@ export default function Intro() {
     return (
         <div className='intro-box'>
             <SwipeableTemporaryDrawer />
-            <div className='intro-container'>
+            <Link to="/omne" className='intro-container' style={{ textDecoration: "none", color: "inherit" }}>
                 <div className='intro-frame' style={{ display: currentFrame === 1 ? 'block' : 'none' }}>
                     <img className='intro-img fade-in' src="./images/intro/1.svg" alt="illustration" />
                 </div>
@@ -44,7 +45,7 @@ export default function Intro() {
                     <img className='intro-img' src="./images/intro/3.svg" alt="illustration" />
                     <img className='intro-img bounce' src="./images/intro/4.svg" alt="illustration" />
                 </div>
-            </div>
+            </Link>
         </div>
     );
    
