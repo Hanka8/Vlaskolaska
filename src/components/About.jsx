97579a7e0c08597fe02aa43a52f3f39f5b10d801
Hanka8 <hanka.maruskevicova@gmail.com>
2024-04-20 22:35:54 +0200

    text updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,10 +15,11 @@ export default function About( ) {
           <span className='visually-hidden'>o mně</span>
         </h1>
         <div className='page-content about'>
-          <p className='text'>Jmenuji se Karolína a pocházím ze Šternberka u Olomouce. Nyní žiji a působím v Praze. Můj zájem o svatby se začal projevovat již na střední škole, avšak mé akademické vzdělání směřovalo jiným směrem, a proto jsem se rozhodla věnovat svou kariéru Armádě České republiky.</p>
-          <p className='text'>Přestože jsem pracovala v odlišném prostředí, mé srdce vždy zůstávalo spojeno se světem svateb. Ve svém volném čase jsem se věnovala pečení dortů, což postupně přerostlo do vášně pro svatební dorty. Mimo to jsem se také začala věnovat úpravám vlasů svých přátel, a to s velkým zápalem a pozorností k detailům.</p>
-          <p className='text'>S přibývajícím časem jsem začala dostávat nabídky na úpravu vlasů a make-up od nevěst, což mě nakonec přivedlo k tomu, abych se plně věnovala oboru svatebního stylingu. Tento proces mi přináší radost a energii, a já se těším, že vám mohu pomoci zazářit ve vašem nejkrásnějším dni.</p>
-          <p className='text'>S láskou a zaujetím se věnuji práci se svatebními účesy a make-upem, abych vám pomohla cítit se sebevědomě a krásně ve vašem výjimečném dni.</p>
+          <p className='text'>Jmenuji se Karolína a pocházím ze Šternberka u Olomouce. Nyní žiji a působím v Praze. Můj zájem o svatby se začal projevovat již na střední škole, nicméně jsem se rozhodla studovat a sloužit v rámci Armády České republiky.</p>
+          <p className='text'>Přestože jsem pracovala v odlišném prostředí, mé srdce vždy zůstávalo spojeno se světem svateb. Ve svém volném čase jsem se věnovala pečení dortů, což postupně přerostlo do vášně pro svatební dorty. Mimo to jsem se také začala s velkým zápalem a pozorností k detailům věnovat úpravám vlasů svých přátel.</p>
+          <p className='text'>S přibývajícím časem a zkušeností jsem od nevěst začala dostávat nabídky na vytváření účesů a make-upu, což mě nakonec přivedlo k tomu, abych se plně věnovala přípravě nevěst, než vykročí k oltáři.</p>
+          <p className='text'>Ráda podtrhnu krásu nevěsty, aby se v její den cítila sebejistě. Rozzářená nevěsta mi přináší radost.</p>
+          <p className='text'>Tímto procesem se mi energie vrací a s láskou tvořím dál.</p>
           <h2 className='h-2'>Absolvované kurzy</h2>
           <ul className='mt-2'>
             <li className='text'>Simeona Lanta Akademie účesů: Kurz individual</li>
--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import SwipeableTemporaryDrawer from './SwipableTemporaryDrawer'
-import { Link } from 'react-router-dom'
 import UseScroll from './hooks/UseScroll'
 
 export default function Process() {
@@ -17,18 +16,19 @@ export default function Process() {
         </h1>
         <div className='page-content process'>
           <h2 className='h-2'>První krok</h2>
-          <p className='text'>Neváhejte se na mě obrátit s dotazy ohledně volného termínu. Můžete mi zavolat nebo napsat na e-mail, který najdete v sekci 
-            <Link to="/kontakt" style={{textDecoration: "none", color: "inherit", marginLeft:".25rem"}}>
-              <a className='link'>Kontakt</a>.
+          <p className='text'>Neváhejte se na mě obrátit s dotazy ohledně volného termínu. Můžete mi napsat na 
+              <a className='link' href="https://api.whatsapp.com/send?phone=732928740">WhatsApp</a>, 
+              <a className='link' hhref="https://www.instagram.com/vlaskolaska/">Instagram</a> nebo
+              <a className='link' href="mailto:karolinacechova22@gmail.com">e-mail</a>.
               <br></br>
-            </Link>
               Vážím si toho, že do mě vkládáte důvěru a ve Váš den se mi svěříte do rukou.<br></br>
               Budu ráda, když mi před prvním setkáním pošlete fotky přibližných představ účesu, případně i líčení.<br></br>
-              Ke zvolení té pravé vizáže pomůže i fotka Vašich vlasů zezadu a Vašich svatebních šatů. Není to však podmínkou.<br></br>
+              Ke zvolení té pravé vizáže pomůže i fotka Vašich vlasů zezadu, Vašich svatebních šatů a svatební kytky. Není to však podmínkou.<br></br>
               Doporučuji už nestříhat vlásky.
           </p>
           <h2 className='h-2'>Zkouška</h2>
-          <p className='text'>Provedení zkoušky může být podceňováno. Nicméně jde o gró naplnění Vašich představ a celkové spokojenosti. Tam se vše „upeče“. Termín zkoušky můžeme domluvit dle časových možností, doporučuji 1-3 týdny před svatbou. Zkouška bude probíhat tam, kde si budete přát Vy.</p>
+          <p className='text'>Provedení zkoušky může být podceňováno. Nicméně jde o gró naplnění Vašich představ a celkové spokojenosti. Tam se vše „upeče“. Termín zkoušky můžeme domluvit dle časových možností, doporučuji 1-3 týdny před svatbou. Zkouška bude probíhat tam, kde si budete přát Vy, trvá přibližně 2,5 - 3,5 hodiny.</p>
+          <p className='text'>Doporučuji si na sebe vzít bílé šaty nebo bílou halenku.</p>
           <p className='text'>Během zkoušky si často s Vaším souhlasem zaznamenávám fotky a videa Vašeho vzhledu, případně různých variant. Ty Vám následně poskytnu, abychom v den svatby docílily maximálního naplnění Vašich představ.</p>
           <p className='text'>Připravte si pro mne informace, kde budou v den D probíhat přípravy, v kolik hodin musíte být v den D připravená na přesun k oltáři.
   Zakládám si na upřímnosti, proto neváhejte kdykoli během zkoušky sdílet Vaše pocity a návrhy na konkrétní úpravu/změnu. Uvítám to. S kůží na trh půjdete Vy, ne já. Je pro mě tedy nejdůležitější, abyste se cítila ve své vizáži svá a byla s výsledkem spokojená.</p>
@@ -39,18 +39,20 @@ export default function Process() {
   Oblíbené jsou také květy ve vlasech. Můžeme se domluvit na konkrétní variantě květin. 
   Pobavíme se i o závoji, pokud ho máte v plánu mít.</p>
           <h3 className='h-3 ml-2'>Líčení</h3>
-          <p className='text ml-2'>Zkouška líčení a účesu trvá 2,5 – 3,5 hod., během kterých si společně probereme, co očekáváte a čemu se chcete naopak vyhnout.
+          <p className='text ml-2'>Společně si probereme, co očekáváte a čemu se chcete naopak vyhnout.
   Setkávám se často s obavami nevěst, že budou příliš zmalované. Mým záměrem není Vám na obličeji vytvořit masku. Chci, abyste se cítila sebejistě a zároveň aby se makeup na fotografiích neztrácel. Najdeme spolu balanc. 
   Mezi značky, které používám patří Mac, Estée Lauder, Bobbi Brown, Huda Beauty, Annabelle Minerals nebo Too faced.</p>
-          <p className='text'><span className='tip'>TIP! </span>Doporučuji si před svatbou (cca týden až dva) zajít na kosmetiku (odkaz na „Doporučuji“), aby byla pleť co nejzdravější a nejlépe nachystaná a bez nežádoucích chloupků. Dle Vašich preferencí zvažte také úpravu obočí. 
+          <p className='text'><span className="tip">TIP! </span>Doporučuji si koupit rtěnku, pro kterou se na zkoušce rozhodneme a dát ji svědkyni, aby Vaše rty byly krásně během celého svatebního dne.</p>
+          <p className='text'><span className='tip'>TIP! </span>Doporučuji si před svatbou (cca týden až dva) zajít na kosmetiku (doporučuji <a className='link' href='https://www.instagram.com/face_c.a.s.e/'>face_c.a.s.e.</a>), aby byla pleť co nejzdravější a nejlépe nachystaná a bez nežádoucích chloupků. Dle Vašich preferencí zvažte také úpravu obočí. 
   Vzhledu vlasů může pomoci maska/kůra na doma z drogerie nebo např. Malibu C u kadeřnice.
   Na zahájení příprav prosím mějte vlasy čisté a hlavně suché (myjte a sušte večer před nebo ráno v den svatby, dejte si záležet se sušením).</p>
+          <p className='text'><span className='tip'>TIP! </span>Vzhledu vlasů může pomoci maska/kůra na doma z drogerie nebo např. Malibu C u kadeřnice.</p>
+          <p className='text'><span className='tip'>TIP! </span>Nezapomeňte na pitný režim a spánek.</p>
           <h2 className='h-2'>Den svatby</h2>
-          <p className='text'>Zvažte, kde chcete, aby probíhaly přípravy. Ideální je místnost, kde je co nejvíce denního světla a zároveň nebudeme rušeny. V každém případě si s sebou vozím i pomocná světla. 
-  Během příprav čas letí a často do nich vstupují další záležitosti, proto je dobré mít časovou rezervu. Nemusíte ale mít obavy, čas příprav Vám ohlídám, Vaše instrukce mám totiž předem poznačené. 
-  Většinou je vhodné si na účes a makeup nevěsty vyčlenit 3 hodiny. 
-  Je vhodné na sobě mít župánek nebo košili, aby převlékání nepoškodilo účes a makeup při převlékání.
-  Na zahájení příprav prosím mějte vlasy čisté a hlavně suché (myjte a sušte večer před nebo ráno v den svatby, dejte si záležet se sušením).</p>
+          <p className='text'>Zvažte, kde chcete, aby probíhaly přípravy. Ideální je místnost, kde nebudeme rušeny, kde je co nejvíce denního světla a přístup k elektrické zásuvce. Pro všechny případy si s sebou vozím i pomocná světla.
+            <br></br>
+            Během příprav čas letí a často do nich vstupují další záležitosti, proto je dobré mít časovou rezervu. Nemusíte ale mít obavy, čas příprav Vám ohlídám, Vaše instrukce mám totiž předem poznačené. Většinou je vhodné si na účes a makeup nevěsty vyčlenit 3 hodiny. Později oceníte, když na sobě mít župánek nebo košili, aby převlékání nepoškodilo účes a makeup při převlékání. Na zahájení příprav prosím mějte vlasy čisté a hlavně suché (myjte a sušte večer před nebo ráno v den svatby, dejte si záležet se sušení).
+          </p>
         </div>
       </div>
     </>
