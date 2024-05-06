548bb26271bed86c104da4ccfc1b659cba6e4421
Hanka8 <hanka.maruskevicova@gmail.com>
2024-05-06 15:00:10 +0200

    link to gallery

--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import SwipeableTemporaryDrawer from './SwipableTemporaryDrawer'
 import UseScroll from './hooks/UseScroll'
+import { Link } from 'react-router-dom'
 
 export default function Process() {
 
@@ -35,7 +36,7 @@ export default function Process() {
           <h3 className='h-3 ml-2'>Účes</h3>
           <p className='text ml-2'>Na zkoušku mějte prosím vlasy čisté a hlavně suché. Moc ráda se s Vámi pobavím o sladění účesu s Vaším obličejem a typem šatů.
   Zvážíme, na co jste zvyklá a jak byste chtěla působit. Sdělím Vám výhody a nevýhody konkrétních variant. Vše můžete v zrcadle kontrolovat a  dát mi upřímnou zpětnou vazbu. Preference jsou zcela na Vás.
-  Je možné si dle zvoleného účesu vybrat a zapůjčit doplňky do vlasů (odkaz k fotkám). Pokud máte svoje, budu ráda, když mi je již na zkoušce ukážete.
+  Je možné si dle zvoleného účesu vybrat a zapůjčit <Link to={"/galerie"} className='link'>doplňky do vlasů.</Link> Pokud máte svoje, budu ráda, když mi je již na zkoušce ukážete.
   Oblíbené jsou také květy ve vlasech. Můžeme se domluvit na konkrétní variantě květin. 
   Pobavíme se i o závoji, pokud ho máte v plánu mít.</p>
           <h3 className='h-3 ml-2'>Líčení</h3>
