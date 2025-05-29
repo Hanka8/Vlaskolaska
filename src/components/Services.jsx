b0d8ce706264d55de2a59f26c6e641f55fe53fe1
Hana Maruškevičová <hanka.maruskevicova@gmail.com>
2025-05-29 21:18:23 +0200

    Update Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -20,19 +20,19 @@ export default function Services() {
               <div>
                   <h2 className='h-2 underline-centered'>Zkouška</h2>
                   <p className='text italic'>(dohromady 2,5 - 3,5 h)</p>
-                  <p className='text mb-02'>svatební líčení: 850,- Kč</p>
-                  <p className='text mb-02'>svatební účes: 1 200,- Kč</p>
+                  <p className='text mb-02'>svatební líčení: 1 500,- Kč</p>
+                  <p className='text mb-02'>svatební účes: 1 700,- Kč</p>
               </div>
               <div>
                   <h2 className='h-2 underline-centered'>Den svatby</h2>
-                  <p className='text mb-02'>svatební líčení: 1200,- Kč</p>
-                  <p className='text'>svatební účes: 1 500,- Kč</p>
+                  <p className='text mb-02'>svatební líčení: 1 600,- Kč</p>
+                  <p className='text'>svatební účes: 1 800,- Kč</p>
               </div>
           </div>
           <div className='content-banner'>
               <p className='text big emphasized mb-02'>Komplet balíček pro nevěsty</p>
               <p className='text mb-02'>(Zkouška účesu, líčení a den svatby)</p>
-              <p className='text big mb-02'>5 400,- Kč</p>
+              <p className='text big mb-02'>6 600,- Kč</p>
           </div>
           <div className='content-grid'>
               <div>
@@ -43,7 +43,7 @@ export default function Services() {
               <div>
                   <h2 className='h-2 underline-centered'>Jiné události</h2>
                   <p className='text italic'>plesy, večírky apod.</p>
-                  <p className='text mb-02'>Líčení: 800,- Kč</p>
+                  <p className='text mb-02'>Líčení: 900,- Kč</p>
                   <p className='text'>Účes: 900 - 1 500,- Kč</p>
               </div>
               <div>
